267281ea01b0d7de0e95c792b957e9f7cf43e5d1
Craig Ambrose <craig@craigambrose.com>
2017-10-22 17:51:26 +1100

    Linked up set_name command, but it doesn't do much

--- app/controllers/messaging_controller.rb
+++ app/controllers/messaging_controller.rb
@@ -1,14 +1,17 @@
 require 'messaging/script'
 require 'messaging/exchange'
+require 'messaging/command_processor'
 
 class MessagingController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
     script = Messaging::Script.from_file script_path(:start)
+    command_processor = Messaging::CommandProcessor.new('Users::Commands')
+
     exchange = Messaging::Exchange.new script: script
     exchange.user_input to: reply[:to], input: reply[:input] if reply
-    exchange.process_commands
+    exchange.process_commands command_processor
     exchange.determine_response
 
     render json: exchange.as_json
---
+++ app/domains/users/commands/set_name.rb
@@ -0,0 +1,9 @@
+module Users
+  module Commands
+    class SetName
+      def call(input)
+        raise "set name with input: #{input.inspect}"
+      end
+    end
+  end
+end
