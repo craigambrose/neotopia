8f9d4d103f88b86934ed658c033e8fef33f6ed13
Craig Ambrose <craig@craigambrose.com>
2017-10-30 21:40:28 +1100

    Some validation on sign up command

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'pg'
 gem 'puma', '~> 3.7'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
+gem 'validates_email_format_of'
 gem 'webpacker'
 
 group :development, :test do
--- app/domains/users/commands/sign_up.rb
+++ app/domains/users/commands/sign_up.rb
@@ -1,20 +1,36 @@
 module Users
   module Commands
     class SignUp
+      include ActiveModel::Validations
+
       class Event < RailsEventStore::Event; end
 
+      attr_accessor :email, :password
+
+      validates :email, presence: true, email_format: true
+      validates :password, presence: true, length: {minimum: 8}
+
       def call(input, context)
-        user_uuid = context.user_uuid
-        publish_user_event user_uuid, Event.new(
-          data: {
-            user_uuid: user_uuid,
-            email: input['email'],
-            encrypted_password: encrypt_password(input['password'])
-          }
-        )
+        self.email = input['email']
+        self.password = input['password']
+
+        if valid?
+          user_uuid = context.user_uuid
+          publish_user_event user_uuid, Event.new(data: event_data)
+        else
+          raise "failed validation with #{errors.inspect}"
+        end
       end
 
       private
+
+      def event_data
+        {
+          user_uuid: user_uuid,
+          email: email,
+          encrypted_password: encrypt_password(password)
+        }
+      end
 
       def publish_user_event(user_uuid, event)
         raise ArgumentError, 'user uuid needed for event publish' if user_uuid.blank?
@@ -22,7 +38,7 @@ module Users
       end
 
       def publish_event(*params)
-        event_store.publish_event *params
+        event_store.publish_event(*params)
       end
 
       def event_store
